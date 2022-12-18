7ea2f09cc251023719b4a4625ff4b0620296322f
DevExpressExampleBot <fakeEmail>
2022-12-18 15:46:23 +0400

    Source auto update [skip ci]

--- VB/NumericRangeControlClient/App.xaml.vb
+++ VB/NumericRangeControlClient/App.xaml.vb
@@ -1,12 +1,3 @@
-﻿' Developer Express Code Central Example:
-' How to display a chart within a range control using the numeric chart client
-' 
-' This example demonstrates how to set up a range control with the numeric chart
-' client to display a chart with numeric data in the range control's background.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5110
-
 ' Developer Express Code Central Example:
 ' How to display a chart within a range control using the numeric chart client
 ' 
@@ -15,7 +6,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5110
-
 ' Developer Express Code Central Example:
 ' How to display a chart within a range control using the numeric chart client
 ' 
@@ -24,21 +14,23 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5110
-
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
-Imports System.Threading.Tasks
+' Developer Express Code Central Example:
+' How to display a chart within a range control using the numeric chart client
+' 
+' This example demonstrates how to set up a range control with the numeric chart
+' client to display a chart with numeric data in the range control's background.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E5110
 Imports System.Windows
 
 Namespace NumericRangeControlClient
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
 
-	End Class
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
 End Namespace
--- VB/NumericRangeControlClient/MainWindow.xaml.vb
+++ VB/NumericRangeControlClient/MainWindow.xaml.vb
@@ -1,17 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace NumericRangeControlClient
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private dataArray() As Integer = { 0, 4, 2, 5, 4, 5, 3, 4, 8, 9, 10 }
+        Private dataArray As Integer() = New Integer() {0, 4, 2, 5, 4, 5, 3, 4, 8, 9, 10}
 
-		Public Sub New()
-			InitializeComponent()
-			DataContext = dataArray
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            DataContext = dataArray
+        End Sub
+    End Class
 End Namespace
